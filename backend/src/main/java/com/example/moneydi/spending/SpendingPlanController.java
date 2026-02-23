e8e24622d9791d5ff81ba57d68c5d006fb1e8712
hyeongwon-up <leehwdatabase@gmail.com>
2026-02-23 22:48:41 +0900

    edit cors config

--- backend/src/main/java/com/example/moneydi/spending/SpendingPlanController.java
+++ backend/src/main/java/com/example/moneydi/spending/SpendingPlanController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/spending-plans")
+@CrossOrigin(origins = "*")
 public class SpendingPlanController {
 
     private final SpendingPlanRepository spendingPlanRepository;
